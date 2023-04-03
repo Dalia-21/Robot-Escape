713e41c3f6d1e468ccb7e1009774e97e4ca140bf
Dalia <dalia.sinclair@protonmail.com>
2023-04-03 12:17:36 +1000

    remove unneeded assets

--- scenes/player.gd
+++ scenes/player.gd
@@ -21,7 +21,7 @@ func _physics_process(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.play("jump")
 	
